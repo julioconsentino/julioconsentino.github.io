23ae96b8c32dbff3a902c373bf6db70d5e7ab853
julioconsentino <julioconsentinoo@gmail.com>
2025-03-08 21:32:57 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -55,3 +55,20 @@
         infoBox.style.visibility = "hidden";
     });
 });
+
+
+   function openInBrowser() {
+        var url = window.location.href;
+
+        // Android (Chrome)
+        var androidUrl = "intent://" + url.replace("https://", "").replace("http://", "") + "#Intent;scheme=https;package=com.android.chrome;end;";
+
+        // iOS & general fallback
+        var fallbackUrl = "https://www.google.com/search?q=" + encodeURIComponent(url);
+
+        if (navigator.userAgent.includes("Android")) {
+            window.location.href = androidUrl;
+        } else {
+            window.open(url, "_blank");
+        }
+    }
