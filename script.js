96b870a18bb0754aa231a9fb732b894a4543fda0
julioconsentino <julioconsentinoo@gmail.com>
2025-03-08 22:19:42 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -57,18 +57,9 @@
 });
 
 
-   function openInBrowser() {
-        var url = window.location.href;
-
-        // Android (Chrome)
-        var androidUrl = "intent://" + url.replace("https://", "").replace("http://", "") + "#Intent;scheme=https;package=com.android.chrome;end;";
-
-        // iOS & general fallback
-        var fallbackUrl = "https://www.google.com/search?q=" + encodeURIComponent(url);
-
-        if (navigator.userAgent.includes("Android")) {
-            window.location.href = androidUrl;
-        } else {
-            window.open(url, "_blank");
-        }
-    }
+function copyLink() {
+    var url = "https://julioconsentino.github.io/";
+    navigator.clipboard.writeText(url).then(() => {
+        alert("Url copiada. Só abrir o navegador (chrome/safari) e colar.");
+    });
+}
